6f2e2367906aa32fd2751d5c746fdcd2fecf75c9
Faolán Hamilton <faolanash01@gmail.com>
2026-05-10 16:24:39 +0000

    trying out the lab

--- labs/train.py
+++ labs/train.py
@@ -5,7 +5,7 @@ url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 page = requests.get(url) 
 doc = parseString(page.content) 
 # check it works 
-print (doc.toprettyxml()) #output to console comment this out once you know it works 
+#print (doc.toprettyxml()) #output to console comment this out once you know it works 
 # if I want to store the xml in a file. You can comment this out later 
 with open("trainxml.xml","w") as xmlfp: 
-doc.writexml(xmlfp) 
+    doc.writexml(xmlfp) 
