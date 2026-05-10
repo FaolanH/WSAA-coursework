05998acb60b6e984acf35b7df3fcef50c937ecc0
Faolán Hamilton <faolanash01@gmail.com>
2026-05-10 21:25:44 +0000

    moving projectetest.py into the project folder

---
+++ project/projecttest.py
@@ -0,0 +1,13 @@
+#Author: Faolán Hamilton
+
+#!flask/bin/python
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
