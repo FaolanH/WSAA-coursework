b739f964fe7749f694871c296a03f3344aab5558
Faolán Hamilton <faolanash01@gmail.com>
2026-05-10 16:43:47 +0000

    trying the train lab, for loop must be within open

--- labs/train.py
+++ labs/train.py
@@ -1,3 +1,4 @@
+
 import requests 
 import csv  
 from xml.dom.minidom import parseString 
@@ -9,3 +10,29 @@ doc = parseString(page.content)
 # if I want to store the xml in a file. You can comment this out later 
 with open("trainxml.xml","w") as xmlfp: 
     doc.writexml(xmlfp) 
+
+retrieveTags=['TrainStatus', 
+'TrainLatitude', 
+'TrainLongitude', 
+'TrainCode', 
+'TrainDate', 
+'PublicMessage', 
+'Direction' 
+] 
+
+objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions") 
+
+# https://stackoverflow.com/questions/18952716/valueerror-i-o-operation-on-closed-file
+#adding the newline= '' parameter 
+with  open('week03_train.csv', mode='w', newline='') as train_file: 
+    train_writer = csv.writer(train_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL) 
+    for objTrainPositionsNode in objTrainPositionsNodes: 
+        traincodenode = objTrainPositionsNode.getElementsByTagName("TrainCode").item(0) 
+        traincode = traincodenode.firstChild.nodeValue.strip()
+    
+    dataList = [] 
+    for retrieveTag in retrieveTags: 
+        datanode = objTrainPositionsNode.getElementsByTagName(retrieveTag).item(0) 
+        dataList.append(datanode.firstChild.nodeValue.strip()) 
+        train_writer.writerow(dataList) 
+        print (dataList)
